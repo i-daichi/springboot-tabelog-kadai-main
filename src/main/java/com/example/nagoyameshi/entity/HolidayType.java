1b035d476126bda7416f6370797e20aa6b52200d
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-05 11:21:21 +0900

    ignoreを修正。 適切なアノテーションを追加

--- src/main/java/com/example/nagoyameshi/entity/HolidayType.java
+++ src/main/java/com/example/nagoyameshi/entity/HolidayType.java
@@ -1,10 +1,16 @@
 package com.example.nagoyameshi.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Entity
+@Table(name = "holiday_types")
+@Data
 public class HolidayType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/nagoyameshi/entity/RestaurantHoliday.java
+++ src/main/java/com/example/nagoyameshi/entity/RestaurantHoliday.java
@@ -1,11 +1,17 @@
 package com.example.nagoyameshi.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Entity
+@Table(name = "restaurant_holidays")
+@Data
 public class RestaurantHoliday {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
