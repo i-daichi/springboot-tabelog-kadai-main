fdaee005ee7fcbf13e4aba324de70268e0b8dff5
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-05 10:57:24 +0900

    IDのfieldの型を修正。

--- src/main/java/com/example/nagoyameshi/entity/Weekday.java
+++ src/main/java/com/example/nagoyameshi/entity/Weekday.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 public class Weekday {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @Column(nullable = false, unique = true)
     private String name;
