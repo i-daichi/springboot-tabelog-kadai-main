c143a1b5cbefced0fb541328ed21f7fe1b74ccdf
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-05 10:55:28 +0900

    IDの型を修正

--- src/main/java/com/example/nagoyameshi/entity/RestaurantHoliday.java
+++ src/main/java/com/example/nagoyameshi/entity/RestaurantHoliday.java
@@ -9,7 +9,7 @@ import jakarta.persistence.ManyToOne;
 public class RestaurantHoliday {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @ManyToOne
     private Restaurant restaurant;
