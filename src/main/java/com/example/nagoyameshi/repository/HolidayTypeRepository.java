c3df82bd49c7647eca105691b9e801d6119df32e
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-05 11:44:51 +0900

    リポジトリにメソッドを追加

--- src/main/java/com/example/nagoyameshi/repository/HolidayTypeRepository.java
+++ src/main/java/com/example/nagoyameshi/repository/HolidayTypeRepository.java
@@ -1,8 +1,11 @@
 package com.example.nagoyameshi.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.nagoyameshi.entity.HolidayType;
+
 @Repository
-public class HolidayTypeRepository {
+public interface HolidayTypeRepository extends JpaRepository<HolidayType, Integer> {
 
 }
--- src/main/java/com/example/nagoyameshi/repository/RestaurantHolidayRepository.java
+++ src/main/java/com/example/nagoyameshi/repository/RestaurantHolidayRepository.java
@@ -1,8 +1,18 @@
 package com.example.nagoyameshi.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class RestaurantHolidayRepository {
+import com.example.nagoyameshi.entity.RestaurantHoliday;
 
+@Repository
+public interface RestaurantHolidayRepository extends JpaRepository<RestaurantHoliday, Integer> {
+    List<RestaurantHoliday> findByRestaurantId(Integer restaurantId);
+
+    @Query("SELECT h FROM RestaurantHoliday h WHERE h.dayOfMonth = :date")
+    List<RestaurantHoliday> findByHolidayDate(@Param("date") Integer date);
 }
