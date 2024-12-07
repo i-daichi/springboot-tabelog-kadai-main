3e06f3680915f95ccb31778d81e97ba1b281b26e
kminamiurawa24 <k.minami.urawa024@gmail.com>
2024-12-07 10:45:16 +0900

    追加

--- src/main/java/com/example/nagoyameshi/dto/RestaurantHolidayDto.java
+++ src/main/java/com/example/nagoyameshi/dto/RestaurantHolidayDto.java
@@ -1,6 +1,5 @@
 package com.example.nagoyameshi.dto;
 
-import com.example.nagoyameshi.entity.HolidayType;
 import com.example.nagoyameshi.entity.Restaurant;
 import com.example.nagoyameshi.entity.RestaurantHoliday;
 import com.example.nagoyameshi.entity.Weekday;
@@ -11,12 +10,10 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class RestaurantHolidayDto {
-    private HolidayType holidayType;
     private Weekday weekday;
     private Integer dayOfMonth; // 例: 毎月15日の場合、"15"
 
     public RestaurantHolidayDto(RestaurantHoliday holiday){
-        this.holidayType = holiday.getHolidayType();
         this.weekday = holiday.getWeekday();
         this.dayOfMonth = holiday.getDayOfMonth();
     }
@@ -24,7 +21,6 @@ public class RestaurantHolidayDto {
     public RestaurantHoliday toRestaurantHoliday(Restaurant restaurant) {
         RestaurantHoliday holiday = new RestaurantHoliday();
         holiday.setRestaurant(restaurant);
-        holiday.setHolidayType(holidayType);
         holiday.setWeekday(weekday);
         holiday.setDayOfMonth(this.dayOfMonth);
         return holiday;
